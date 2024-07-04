654af4688f4e2b919d9638d1b5ec29ec28742c71
Daniel Oladepo <oladepodaniel69@gmail.com>
2024-07-04 14:00:23 +0100

    Revamped the collabocate repo

---
+++ server/src/app.ts
@@ -0,0 +1,41 @@
+import 'express-async-errors';
+import express, { Express, NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import cors from 'cors';
+import { errorHandler } from './lib/errors/ErrorHandler';
+import { CustomErrorInterface } from './lib/errors/CustomError';
+import { notFoundErr } from './lib/errors/Errors';
+import { router as appRouter } from './api/routes/app.route';
+
+dotenv.config();
+
+const app: Express = express();
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(express.json());
+app.use(cors({ origin: [`http://localhost:${process.env.CLIENT_APP_PORT}`, `${process.env.CLIENT_APP_URL}`] }));
+
+//====== Use Routers =======
+app.use('/', appRouter);
+//==========================
+
+
+//========= Throw Route Not Found Error ==========
+app.use(() => {
+  notFoundErr("Route Not Found")
+});
+//==========================================
+
+
+//====== Error handler Middleware ==========
+app.use((err: CustomErrorInterface, req: Request, res: Response, next: NextFunction) => {
+  errorHandler.handleError(err, res);
+  next()
+});
+//==========================================
+
+export { app };
