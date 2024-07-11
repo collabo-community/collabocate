b6fa4872ab00625f71cd6003aabbac39cbc11a7f
Ifycode <marychord@gmail.com>
2024-07-11 01:57:39 +0100

    Move api server settings to repo root

--- backend-server/server.ts
+++ backend-server/server.ts
@@ -1,7 +1,7 @@
 import { app as app} from './app'
 
 
-const PORT = process.env.PORT || '3000';
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
