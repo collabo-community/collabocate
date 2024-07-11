8afc67b3f9342bff7cfd6ea7a4e3b3d8dc080e00
Ifycode <marychord@gmail.com>
2024-07-11 04:39:08 +0100

    Fix: Move fetch into service file and revamp response in controller

--- @api-server/api/app.controller.ts
+++ @api-server/api/app.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import { success } from '../lib/helpers';
+import { createIssueService, getIssuesService, getPullRequestsService, getRepositoriesService } from './app.service';
 
 
 export interface ApiInfo {
@@ -32,85 +34,53 @@ export const getAppController =  async (req: Request, res: Response) => {
     res.status(200).json(apiInfo);
 }
 
+
+let response: { [key: string]: unknown } = {};
+const message = {
+  success: {
+    get: 'GET request successful!',
+    issues: {
+      submitted: 'Issue ticket successfully submitted!',
+    },
+  },
+}
+
 export const getIssuesController =  async (req: Request, res: Response) => {
-  try {
-    const response = await fetch(`${process.env.REPO_API_URL}/issues`, {
-      headers: {
-        Authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-    if (!response.ok) {
-      throw new Error(data.message || "Error fetching issues");
-    }
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Error fetching issues" });
+  const docs = await getIssuesService();
+  response = {
+    count: docs.length,
+    issues: docs,
   }
+  success(message.success.get);
+  return res.status(200).json(response);
 }
 
 export const createIssueController =  async (req: Request, res: Response) => {
-  const { title, body } = req.body;
-  try {
-    const response = await fetch(`${process.env.REPO_API_URL}/issues`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
-      },
-      body: JSON.stringify({ 
-        title: `[GitHubSync] ${title}`,
-        body: body + '\n\n' + '#' + '\n' + '> Submitted via **Collabocate** [[GitHubSync]](https://github.com/collabo-community/collabocate)',
-      }),
-    });
-
-    if (!response.ok) {
-      const data = await response.json();
-      throw new Error(
-        data.message || `Failed to create issue! Status: ${response.status}`
-      );
-    }
-
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Error creating issue" });
+  const docs = await createIssueService(req);
+  response = {
+    message,
+    issue: docs,
   }
+  success(message.success.issues.submitted);
+  return res.status(201).json(response);
 }
 
 export const getPullRequestsController =  async (req: Request, res: Response) => {
-  try {
-    const response = await fetch(`${process.env.REPO_API_URL}/pulls`, {
-      headers: {
-        Authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-    if (!response.ok) {
-      throw new Error(data.message || "Error fetching pull requests");
-    }
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Error fetching pull requests" });
+  const docs = await getPullRequestsService();
+  response = {
+    count: docs.length,
+    pull_requests: docs,
   }
+  success(message.success.get);
+  return res.status(200).json(response);
 }
 
 export const getRepositoriesController =  async (req: Request, res: Response) => {
-  try {
-    const response = await fetch(
-      `${process.env.GITHUB_API_BASE_URL}/user/repos`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
-        },
-      }
-    );
-    const data = await response.json();
-    if (!response.ok) {
-      throw new Error(data.message || "Error fetching repositories");
-    }
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Error fetching repositories" });
+  const docs = await getRepositoriesService();
+  response = {
+    count: docs.length,
+    repositories: docs,
   }
+  success(message.success.get);
+  return res.status(200).json(response);
 }
