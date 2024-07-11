949fbebfa29a28ad00a90f7032060a50a860912c
Ifycode <marychord@gmail.com>
2024-07-11 02:16:51 +0100

    Fix: Rename to client, fix import issues with esm package

--- client/server.js
+++ client/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(express.static("public"));
+app.use(express.static("client"));
 
 const PORT = process.env.CLIENT_APP_PORT || 8080;
 
