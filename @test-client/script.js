d3f625996e04d6cc095cda76ca290cfd5784a57a
KBandipo~ <keneebandipo@gmail.com>
2024-08-05 10:57:49 +0100

    Restore the use of double quote to single and remove the console logs to make the code clean

--- @test-client/script.js
+++ @test-client/script.js
@@ -1,8 +1,8 @@
-import config from "./lib/load.config.js";
+import config from './lib/load.config.js';
 
-const submitIssueForm = document.getElementById("submitIssueForm");
+const submitIssueForm = document.getElementById('submitIssueForm');
 
-const displayToastrMessage = document.getElementById("displayToastrMessage");
+const displayToastrMessage = document.getElementById('displayToastrMessage');
 
 /* ------------------------------
   Fetch repositories on page load
@@ -11,7 +11,7 @@ try {
   const response = await fetch(`${config.backend_url}/repositories`);
   const data = await response.json();
   console.log(data);
-  displayToastrMessage.innerHTML = "Fetch successful!";
+  displayToastrMessage.innerHTML = 'Fetch successful!';
 } catch (error) {
   displayToastrMessage.innerHTML = `An error occurred. ${error.message}.`;
 }
@@ -19,33 +19,32 @@ try {
 /* --------------------------------
   Submit an Issue ticket through UI
 -------------------------------- */
-submitIssueForm.addEventListener("submit", async (e) => {
+submitIssueForm.addEventListener('submit', async (e) => {
   try {
     e.preventDefault();
-    console.log(e);
-    const issueTitleInput = document.getElementById("issueTitle");
-    const issueBodyInput = document.getElementById("issueBody");
+    const issueTitleInput = document.getElementById('issueTitle');
+    const issueBodyInput = document.getElementById('issueBody');
     const title = issueTitleInput.value.trim();
     const body = issueBodyInput.value.trim();
     if (!title || !body) {
-      displayToastrMessage.innerHTML = "Issue Title and Body cannot be empty.";
+      displayToastrMessage.innerHTML = 'Issue Title and Body cannot be empty.';
       return;
     }
 
     const response = await fetch(`${config.backend_url}/issues`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ title, body }),
     });
     const data = await response.json();
     console.log(data);
-    issueTitleInput.value = "";
-    issueBodyInput.value = "";
+    issueTitleInput.value = '';
+    issueBodyInput.value = '';
     displayToastrMessage.innerHTML = `${data.message} Follow your issue ticket's progress here: ${data.issue.html_url}`;
   } catch (error) {
     displayToastrMessage.innerHTML =
-      "An error occurred. Could not submit issue ticket.";
+      'An error occurred. Could not submit issue ticket.';
   }
 });
