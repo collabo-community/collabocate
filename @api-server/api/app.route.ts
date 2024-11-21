b34883fe576ce7baf4a3566923c34f16b6f7684c
KBandipo~ <keneebandipo@gmail.com>
2024-11-21 16:42:15 +0100

    Feat: Add issue-templates route

--- @api-server/api/app.route.ts
+++ @api-server/api/app.route.ts
@@ -1,5 +1,5 @@
 import express, { IRouter } from 'express';
-import { createIssueController, getAppController, getIssuesController, getPullRequestsController, getRepositoriesController } from './app.controller';
+import { createIssueController, getAppController, getIssuesController, getPullRequestsController, getRepositoriesController,getIssueTemplatesController } from './app.controller';
 
 const router: IRouter = express.Router();
 
@@ -8,6 +8,7 @@ router.get('/issues', getIssuesController);
 //-------------------------------------------
 router.post('/issues', createIssueController);
 //-------------------------------------------
+router.get('/issue-templates', getIssueTemplatesController);
 router.get('/pull-requests', getPullRequestsController);
 router.get('/repositories', getRepositoriesController);
 
